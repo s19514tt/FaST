80ec8ebbfd0f5e103a932fc715d8e8bc98fbd630
s19514tt <s19514tt@sfc.keio.ac.jp>
2022-07-23 22:39:30 +0900

    changed parser code

--- src/parser/html_parser.ts
+++ src/parser/html_parser.ts
@@ -2,8 +2,22 @@ import { minify } from 'html-minifier'
 import parse, { HTMLElement, Node } from 'node-html-parser'
 
 export function parseHtml(html: string): HTMLElement {
-  // const minifiedHtml = minify(html)
-  const parsedHtml = parse(html)
-  console.log((parsedHtml.childNodes[1] as HTMLElement).rawAttributes)
+  const minifiedHtml = minify(html, {
+    collapseWhitespace: true,
+  })
+  const parsedHtml = parse(minifiedHtml)
+  console.log((parsedHtml as HTMLElement).attributes)
+  ;(parsedHtml as HTMLElement).setAttribute('a', 'b')
+  console.log((parsedHtml as HTMLElement).attributes)
+  console.log((parsedHtml as HTMLElement).toString())
+  console.log(minifiedHtml)
+  const a = parse("<p id='show1' :if='show1'>${message+message2}</p>")
+  //@ts-ignore
+  parsedHtml.childNodes[0].setAttribute('a', 'b')
+  //@ts-ignore
+  console.log(parsedHtml.childNodes[0].attrs)
+  console.log(parsedHtml.childNodes[0].toString())
+  //@ts-ignore
+  console.log(a.childNodes[0].attrs)
   return parsedHtml
 }
